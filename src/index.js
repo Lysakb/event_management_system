73ae3fca016d3f0258d3deef1028e05ed64a895e
Elizabeth Borokinni <elizabethborokinni@gmail.com>
2023-03-10 23:46:18 +0100

    user route

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const {connectToMongodb} = require("../src/database/database");
+const userRoute = require("../src/route/user.route");
 require("dotenv").config();
 
 connectToMongodb();
 
 const PORT = process.env.PORT
 const app = express();
+
+app.use(express.json());
+app.use("/", userRoute);
 
 app.get("/", (req, res)=>{
     res.status(200).send("Hello")
