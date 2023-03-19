8def59996739d052daa7efa69c501972115366f4
Elizabeth Borokinni <elizabethborokinni@gmail.com>
2023-03-19 21:16:50 +0100

    edit status

--- src/route/event.route.js
+++ src/route/event.route.js
@@ -4,15 +4,15 @@ const {createEvent, getEvents, getEventsById, updateEvents, deleteEvents, Addatt
 const authentication = require("../middleware/authentication");
 const authorization = require("../middleware/authorization");
 
-eventRoute.post("/add", createEvent);
+eventRoute.post("/add", authentication, createEvent);
 eventRoute.get("/", getEvents);
 eventRoute.get("/:id", authentication, getEventsById);
 eventRoute.put("/update/:id", authentication, updateEvents);
 eventRoute.delete("/delete/:id", authentication, deleteEvents);
 eventRoute.post("/add-to-event/:id", authentication, Addattendee);
 eventRoute.get("/get-stats/:id", authentication, getEventStats); 
-// eventRoute.put("/change-status/:id", authentication, authorization, changeStatus);
+eventRoute.put("/change-status/:id", authentication, authorization, changeStatus);
 
 
 
-module.exports = eventRoute;
+module.exports = eventRoute; 
