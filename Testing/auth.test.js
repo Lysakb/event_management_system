bdd561e8342abd9b39f1bbb8fe123e8d9313ea75
Barny Victor <victorbarny4@gmail.com>
2023-03-28 17:56:29 +0100

    auth test

---
+++ Testing/auth.test.js
@@ -0,0 +1,45 @@
+const request = require("supertest");
+const { connect } = require("./database");
+const app = require("../src/index");
+const userModel = require("../src/model/user");
+
+describe("Auth route test", () => {
+  let conn;
+
+  beforeAll(async () => {
+    conn = await connect();
+  });
+
+  afterEach(async () => {
+    await conn.cleanup();
+  });
+
+  afterAll(async () => {
+    await conn.disconnect();
+  });
+
+  it("should signup a user ", async () => {
+    const response = await request(app).post("/user/register").send({
+      name: "victor",
+      password: "Password123",
+      email: "victor@mail.com",
+      role: "user",
+    });
+    expect(response.status).toBe(200);
+  });
+
+  it("should login a user", async () => {
+    const user = await userModel.create({
+      name: "Gift",
+      password: "Password123",
+      email: "Gift@mail.com",
+      role: "user",
+    });
+
+    const response = await request(app).post("/user/login").send({});
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("token");
+    expect(response.body).toHaveProperty("message");
+  });
+});
